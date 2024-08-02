b91ccf8445e161706a2fc8704e6c9bd4ff350d30
theo.lalande <theo.lalande@compufirst.com>
2024-08-02 11:48:21 +0200

    ajout de lanim des badges

--- src/app/components/Projects/Project.jsx
+++ src/app/components/Projects/Project.jsx
@@ -18,54 +18,64 @@ const Project = (props) => {
   };
   return (
     <div className="flex flex-row gap-5 justify-center items-center ">
-      <div
-        className="relative w-48 sm:w-72 md:w-96 md:h-72 h-48 border-black border-[1px] border-solid rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:shadow-[0px_10px_1px_rgba(50,_50,_50,_0.1),_0_10px_20px_rgba(34,_34,_34,_0.5)] overflow-hidden duration-500"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-      >
-        <Image
-          src={props.thumbnail}
-          alt={props.thumbnail}
-          fill
-          className="object-cover hover:blur-sm duration-500"
-          priority
-        />
-        {isHovered ? (
-          <div>
-            <p
-              className={`transition-opacity opacity-100 w-full flex justify-center absolute top-10 left-auto text-center duration-500 text-lg ${props.titleColor}`}
-            >
-              {props.projectName}
-            </p>
-            <button
-              onClick={handleOpenDialog}
-              className="text-sm transition-opacity opacity-100 duration-500 absolute bottom-0 left-0 w-full h-6 bg-black bg-opacity-40 text-white text-center"
-            >
-              Voir le projet
-            </button>
-          </div>
-        ) : (
-          <div>
-            <p className="transition-opacity opacity-0 w-full flex justify-center absolute top-10 left-auto text-center duration-500 text-lg">
-              {props.projectName}
-            </p>
-            <button className="text-sm transition-opacity opacity-0 duration-500 absolute bottom-0 left-0 w-full h-6 bg-black bg-opacity-40 text-white text-center">
-              Voir le projet
-            </button>
-          </div>
-        )}
+      <div className="relative">
+        {/* LES BADGES */}
+        <div
+          className={`flex flex-col gap-2 absolute top-0 ${
+            isHovered ? "-right-20" : "right-0"
+          } transition-right  z-0`}
+        >
+          {props.badges.map((badge, index) => {
+            return (
+              <Badges
+                key={index}
+                borderColor={badge.borderColor}
+                text={badge.text}
+              />
+            );
+          })}
+        </div>
+
+        {/* LES PROJETS */}
+        <div
+          className="relative w-48 sm:w-72 md:w-96 md:h-72 h-48 border-black border-[1px] border-solid rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] hover:shadow-[0px_10px_1px_rgba(50,_50,_50,_0.1),_0_10px_20px_rgba(34,_34,_34,_0.5)] overflow-hidden duration-500 z-10"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <Image
+            src={props.thumbnail}
+            alt={props.thumbnail}
+            fill
+            className="object-cover hover:blur-sm duration-500"
+            priority
+          />
+          {isHovered ? (
+            <div>
+              <p
+                className={`transition-opacity opacity-100 w-full flex justify-center absolute top-10 left-auto text-center duration-500 text-lg ${props.titleColor}`}
+              >
+                {props.projectName}
+              </p>
+              <button
+                onClick={handleOpenDialog}
+                className="text-sm transition-opacity opacity-100 duration-500 absolute bottom-0 left-0 w-full h-6 bg-black bg-opacity-40 text-white text-center"
+              >
+                Voir le projet
+              </button>
+            </div>
+          ) : (
+            <div>
+              <p className="transition-opacity opacity-0 w-full flex justify-center absolute top-10 left-auto text-center duration-500 text-lg">
+                {props.projectName}
+              </p>
+              <button className="text-sm transition-opacity opacity-0 duration-500 absolute bottom-0 left-0 w-full h-6 bg-black bg-opacity-40 text-white text-center">
+                Voir le projet
+              </button>
+            </div>
+          )}
+        </div>
       </div>
-      <div className="flex flex-col gap-2">
-        {props.badges.map((badge, index) => {
-          return (
-            <Badges
-              key={index}
-              borderColor={badge.borderColor}
-              text={badge.text}
-            />
-          );
-        })}
-      </div>
+      {/* LA POPUP QUI SOUVRIRA SI ON CLIQUE SUR LE PROJET */}
       <GaleryModal
         isDialogOpen={isDialogOpen}
         projectName={props.projectName}
--- src/app/components/common/Navbar.jsx
+++ src/app/components/common/Navbar.jsx
@@ -4,9 +4,6 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="text-black flex flex-row justify-between items-center pt-6 px-10 w-full">
-      <a href="/" className="flex w-1/3 text-base justify-start ">
-        Stack
-      </a>
       <div className="relative flex w-10 h-10  sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20">
         <Image
           src="/logo2.svg"
@@ -16,9 +13,6 @@ const Navbar = () => {
           priority
         />
       </div>
-      <a href="/" className=" flex w-1/3 text-base justify-end ">
-        Contact
-      </a>
     </div>
   );
 };
